0c12790b5538e6f9fed8847056f7c153abab9153
Abhishek Mulasi <abhishekmulasi@gmail.com>
2024-10-06 12:52:06 -0700

    Added SQLite3 as a database

--- Phase 2/Code/Task_0a.py
+++ Phase 2/Code/Task_0a.py
@@ -2,6 +2,9 @@ import os
 import shutil
 import json
 
+# import SQL library
+import sqlite3
+
 # import the python libraries
 import cv2
 import torch
@@ -19,119 +22,145 @@ target_videos = ['golf',  'shoot_ball', 'brush_hair', 'handstand', 'shoot_bow',
                 'jump', 'situp', 'chew', 'kick', 'smile', 'clap', 'kick_ball', 'smoke',
                 'climb', 'somersault', 'climb_stairs', 'laugh', 'stand']
 
+# Connect to the database
+connection = sqlite3.connect('../database/Phase_2.db')
+c = connection.cursor()
+
+# Create database Table
+create_table = """CREATE TABLE IF NOT EXISTS data (
+                    videoID INTEGER PRIMARY KEY,
+                    Video_Name VARCHAR(250),
+                    Layer_3 VARCHAR(4000),
+                    Layer_4 VARCHAR(4000),
+                    AvgPool VARCHAR(4000),
+                    BOF_HOG VARCHAR(4000),
+                    BOF_HOF VARCHAR(4000),
+                    Action_Label VARCHAR(20));"""
+
+# TODO: create table for P1_Task3
+# create_color_hist_table = """CREATE TABLE FI NOT EXISTS color_hist_data ();"""
+
+c.execute(create_table)
+# c.execute(create_color_hist_table)
+
+
 # Dictionary to map video name to videoID
 videoID = {}
 
 def create_video_id():
     counter = 0
-    target_path = '../target_videos'
-    non_target_path = '../non_target_videos'
+    target_path = '../dataset/target_videos'
+    non_target_path = '../dataset/non_target_videos'
     for file in os.listdir(target_path):
         videoID[file] = counter
         counter += 1
     for file in os.listdir(non_target_path):
         videoID[file] = counter
         counter += 1
+    
+    # JSON is used as a backup to data
+    with open('../database/videoID.json', 'w') as f:
+        json.dump(videoID, f, indent=4)
+
 
 # Dictionary to map video name to Action
 datamap = {}
 
 def video_category_map():
-    dataset = '../hmdb51_org-1'
+    dataset = '../hmdb51_org'
     for action in os.listdir(dataset):
         action_path = os.path.join(dataset, action)
-        if action == '.DS_Store':
-            continue
-        else:
+        if action in target_videos:
             for file in os.listdir(action_path):
-                a = {}
-                a[file] = action
-                datamap.update(a)
+                if videoID[file]%2 == 0:
+                    a = {}
+                    a[file] = action
+                    datamap.update(a)
+        elif action == '.DS_Store':
+            continue
+    
+    # JSON is used as a backup to data
     with open('../database/category_map.json', 'w') as f:
         json.dump(datamap, f, indent=4)
 
-def target_even_videos_features():
-    target_path = '../target_videos'
-    # TODO: layer_output can be a dictionary instead of an array
+def target_videos_features():
+    target_path = '../dataset/target_videos'
     layer_output = []
     for target_videos in os.listdir(target_path):
         video_path = os.path.join(target_path, target_videos)
-        if videoID[target_videos]%2 == 0:
-            a = {}
-            features_video =[]
-            features_video.append(layer3_feature(video_path))
-            features_video.append(layer4_feature(video_path))
-            features_video.append(avgpool_feature(video_path))
-            # TODO: Add Task2 Features
-            # TODO: Add Task3 Features
-            features_video.append(datamap[target_videos])
 
-            a[target_videos] = features_video
-            layer_output.append(a)
-
-    with open('../database/target_even_features.json', 'w') as f:
-        json.dump(layer_output, f, indent=4)
-
-def target_odd_videos_features():
-    target_path = '../target_videos'
-    # TODO: layer_output can be a dictionary instead of an array
-    layer_output = []
-    for target_videos in os.listdir(target_path):
-        video_path = os.path.join(target_path, target_videos)
-        if videoID[target_videos]%2 != 0:
-            a = {}
-            features_video =[]
-            features_video.append(layer3_feature(video_path))
-            features_video.append(layer4_feature(video_path))
-            features_video.append(avgpool_feature(video_path))
-            # TODO: Add Task2 Features
-            # TODO: Add Task3 Features
-
-            a[target_videos] = features_video
-            layer_output.append(a)
-
-    with open('../database/target_odd_features.json', 'w') as f:
-        json.dump(layer_output, f, indent=4)
-
-def non_target_videos_features():
-    non_target_path = '../non_target_videos'
-    # TODO: layer_output can be a dictionary instead of an array
-    layer_output = []
-    for non_target_videos in os.listdir(non_target_path):
-        video_path = os.path.join(non_target_path, non_target_videos)
         a = {}
         features_video =[]
-        features_video.append(layer3_feature(video_path))
-        features_video.append(layer4_feature(video_path))
-        features_video.append(avgpool_feature(video_path))
+
+        layer_3 = layer3_feature(video_path)
+        features_video.append(layer_3)
+        layer_4 = layer4_feature(video_path)
+        features_video.append(layer_4)
+        avgPool = avgpool_feature(video_path)
+        features_video.append(avgPool)
         # TODO: Add Task2 Features
         # TODO: Add Task3 Features
+
         a[target_videos] = features_video
         layer_output.append(a)
 
-    with open('../database/non_target_odd_features.json', 'w') as f:
+        if videoID[target_videos]%2 == 0:
+            video_insert = f"INSERT INTO data VALUES({videoID[target_videos]}, '{target_videos}', '{layer_3}', '{layer_4}', '{avgPool}', '[]', '[]', '{datamap[target_videos]}');"
+        else:
+            video_insert = f"INSERT INTO data VALUES({videoID[target_videos]}, '{target_videos}', '{layer_3}', '{layer_4}', '{avgPool}', '[]', '[]', NULL);"
+        c.execute(video_insert)
+    
+    with open('../database/total_target_features.json', 'w') as f:
         json.dump(layer_output, f, indent=4)
 
-# Move all videos from 'hmdb51_org' to 'target_videos' and 'non_target_videos'
+def non_target_videos_features():
+    non_target_path = '../dataset/non_target_videos'
+    layer_output = []
+    for non_target_videos in os.listdir(non_target_path):
+        video_path = os.path.join(non_target_path, non_target_videos)
+
+        a = {}
+        features_video =[]
+
+        layer_3 = layer3_feature(video_path)
+        features_video.append(layer_3)
+        layer_4 = layer4_feature(video_path)
+        features_video.append(layer_4)
+        avgPool = avgpool_feature(video_path)
+        features_video.append(avgPool)
+        # TODO: Add Task2 Features
+        # TODO: Add Task3 Features
+
+        a[target_videos] = features_video
+        layer_output.append(a)
+
+        video_insert = f"INSERT INTO data VALUES({videoID[target_videos]}, '{target_videos}', '{layer_3}', '{layer_4}', '{avgPool}', '[]', '[]', NULL);"
+        c.execute(video_insert)
+
+    with open('../database/total_non_target_features.json', 'w') as f:
+        json.dump(layer_output, f, indent=4)
+
+
+# Move all videos from 'hmdb51_org' to 'dataset/target_videos' and 'dataset/non_target_videos'
 def move_target_videos():
     path = '../hmdb51_org'
 
-    target_path = os.listdir('../target_videos')
+    target_path = os.listdir('../dataset/target_videos')
     for file in target_path:
-        full_path = os.path.join('../target_videos', file)
+        full_path = os.path.join('../dataset/target_videos', file)
         if os.path.isfile(full_path):
             os.remove(full_path)
 
-    non_target_path = os.listdir('../non_target_videos')
+    non_target_path = os.listdir('../dataset/non_target_videos')
     for file in non_target_path:
-        full_path = os.path.join('../non_target_videos', file)
+        full_path = os.path.join('../dataset/non_target_videos', file)
         if os.path.isfile(full_path):
             os.remove(full_path)
 
     for video_dir in os.listdir(path):
         if video_dir in target_videos:
             video_path = f'{path}/{video_dir}'
-            destination_dir = '../target_videos'
+            destination_dir = '../dataset/target_videos'
             for all_videos in os.listdir(video_path):
                 file_name = os.path.join(video_path, all_videos)
                 shutil.copy(file_name, destination_dir)
@@ -139,16 +168,21 @@ def move_target_videos():
             continue
         else:
             video_path = f'{path}/{video_dir}'
-            destination_dir = '../non_target_videos'
+            destination_dir = '../dataset/non_target_videos'
             for all_videos in os.listdir(video_path):
                 file_name = os.path.join(video_path, all_videos)
                 shutil.copy(file_name, destination_dir)
 
     create_video_id()
     video_category_map()
-    target_even_videos_features()
-    target_odd_videos_features()
+    target_videos_features()
     non_target_videos_features()
 
 
 move_target_videos()
+
+# commit the changes
+connection.commit()
+
+# close the Database connection
+connection.close()
