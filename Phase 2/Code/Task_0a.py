75261b3892e590607b1c978cc985cf4e7269cf68
Abhishek Mulasi <abhishekmulasi@gmail.com>
2024-10-06 22:06:16 -0700

    Added HoG, HoF values for Phase 2. Added Util folder

--- Phase 2/Code/Task_0a.py
+++ Phase 2/Code/Task_0a.py
@@ -13,21 +13,33 @@ import torchvision.models as models
 import torch.nn as nn
 
 # import functions to generate feature spaces
-from Task_1 import layer3_feature
-from Task_1 import layer4_feature
-from Task_1 import avgpool_feature
+from Util.Task_1 import layer3_feature
+from Util.Task_1 import layer4_feature
+from Util.Task_1 import avgpool_feature
+from Util.Task_2 import get_cluster_representatives
+from Util.Task_2 import get_HoG_HoF_features
 
 target_videos = ['golf',  'shoot_ball', 'brush_hair', 'handstand', 'shoot_bow', 
                 'cartwheel', 'hit', 'shoot_gun', 'hug', 'sit', 'catch', 
-                'jump', 'situp', 'chew', 'kick', 'smile', 'clap', 'kick_ball', 'smoke',
+                'jump', 'situp', 'chew', 'kick', 'smile', 'clap', 'kick_ball', 'smoke', 
                 'climb', 'somersault', 'climb_stairs', 'laugh', 'stand']
+
+# Dictionary to map video name to videoID
+videoID = {}
+# Dictionary to map video name to Action
+actionmap = {}
+
+# Dictionaries to map (tau, sigma) values to Cluster Representatives
+HoG = {}
+HoF = {}
+
 
 # Connect to the database
 connection = sqlite3.connect('../database/Phase_2.db')
 c = connection.cursor()
 
-# Create database Table
-create_table = """CREATE TABLE IF NOT EXISTS data (
+# Create database Tables
+create_data_table = """CREATE TABLE IF NOT EXISTS data (
                     videoID INTEGER PRIMARY KEY,
                     Video_Name VARCHAR(250),
                     Layer_3 VARCHAR(4000),
@@ -38,14 +50,11 @@ create_table = """CREATE TABLE IF NOT EXISTS data (
                     Action_Label VARCHAR(20));"""
 
 # TODO: create table for P1_Task3
-# create_color_hist_table = """CREATE TABLE FI NOT EXISTS color_hist_data ();"""
+# create_color_hist_table = """CREATE TABLE IF NOT EXISTS color_hist_data ();"""
 
-c.execute(create_table)
+c.execute(create_data_table)
 # c.execute(create_color_hist_table)
 
-
-# Dictionary to map video name to videoID
-videoID = {}
 
 def create_video_id():
     counter = 0
@@ -63,9 +72,6 @@ def create_video_id():
         json.dump(videoID, f, indent=4)
 
 
-# Dictionary to map video name to Action
-datamap = {}
-
 def video_category_map():
     dataset = '../hmdb51_org'
     for action in os.listdir(dataset):
@@ -75,13 +81,14 @@ def video_category_map():
                 if videoID[file]%2 == 0:
                     a = {}
                     a[file] = action
-                    datamap.update(a)
+                    actionmap.update(a)
         elif action == '.DS_Store':
             continue
     
     # JSON is used as a backup to data
     with open('../database/category_map.json', 'w') as f:
-        json.dump(datamap, f, indent=4)
+        json.dump(actionmap, f, indent=4)
+
 
 def target_videos_features():
     target_path = '../dataset/target_videos'
@@ -98,20 +105,23 @@ def target_videos_features():
         features_video.append(layer_4)
         avgPool = avgpool_feature(video_path)
         features_video.append(avgPool)
-        # TODO: Add Task2 Features
+        bof_HOG, bof_HOF = get_HoG_HoF_features('../dataset_stips/target_videos/' + target_videos + '.txt')
+        features_video.append(bof_HOG)
+        features_video.append(bof_HOF)
         # TODO: Add Task3 Features
 
         a[target_videos] = features_video
         layer_output.append(a)
 
         if videoID[target_videos]%2 == 0:
-            video_insert = f"INSERT INTO data VALUES({videoID[target_videos]}, '{target_videos}', '{layer_3}', '{layer_4}', '{avgPool}', '[]', '[]', '{datamap[target_videos]}');"
+            video_insert = f"INSERT INTO data VALUES({videoID[target_videos]}, '{target_videos}', '{layer_3}', '{layer_4}', '{avgPool}', '{bof_HOG}', '{bof_HOF}', '{actionmap[target_videos]}');"
         else:
-            video_insert = f"INSERT INTO data VALUES({videoID[target_videos]}, '{target_videos}', '{layer_3}', '{layer_4}', '{avgPool}', '[]', '[]', NULL);"
+            video_insert = f"INSERT INTO data VALUES({videoID[target_videos]}, '{target_videos}', '{layer_3}', '{layer_4}', '{avgPool}', '{bof_HOG}', '{bof_HOF}', NULL);"
         c.execute(video_insert)
     
     with open('../database/total_target_features.json', 'w') as f:
         json.dump(layer_output, f, indent=4)
+
 
 def non_target_videos_features():
     non_target_path = '../dataset/non_target_videos'
@@ -128,13 +138,15 @@ def non_target_videos_features():
         features_video.append(layer_4)
         avgPool = avgpool_feature(video_path)
         features_video.append(avgPool)
-        # TODO: Add Task2 Features
+        bof_HOG, bof_HOF = get_HoG_HoF_features('../dataset_stips/non_target_videos/' + target_videos + '.txt')
+        features_video.append(bof_HOG)
+        features_video.append(bof_HOF)
         # TODO: Add Task3 Features
 
         a[target_videos] = features_video
         layer_output.append(a)
 
-        video_insert = f"INSERT INTO data VALUES({videoID[target_videos]}, '{target_videos}', '{layer_3}', '{layer_4}', '{avgPool}', '[]', '[]', NULL);"
+        video_insert = f"INSERT INTO data VALUES({videoID[target_videos]}, '{target_videos}', '{layer_3}', '{layer_4}', '{avgPool}', '{bof_HOG}', '{bof_HOF}', NULL);"
         c.execute(video_insert)
 
     with open('../database/total_non_target_features.json', 'w') as f:
@@ -142,7 +154,7 @@ def non_target_videos_features():
 
 
 # Move all videos from 'hmdb51_org' to 'dataset/target_videos' and 'dataset/non_target_videos'
-def move_target_videos():
+def move_videos():
     path = '../hmdb51_org'
 
     target_path = os.listdir('../dataset/target_videos')
@@ -173,13 +185,48 @@ def move_target_videos():
                 file_name = os.path.join(video_path, all_videos)
                 shutil.copy(file_name, destination_dir)
 
-    create_video_id()
-    video_category_map()
-    target_videos_features()
-    non_target_videos_features()
+
+# Move all video STIPs from 'hmdb51_org_stips' to 'dataset_stips/target_videos' and 'dataset_stips/non_target_videos'
+def move_stips():
+    path = '../hmdb51_org_stips'
+
+    target_path = os.listdir('../dataset_stips/target_videos')
+    for file in target_path:
+        full_path = os.path.join('../dataset_stips/target_videos', file)
+        if os.path.isfile(full_path):
+            os.remove(full_path)
+
+    non_target_path = os.listdir('../dataset_stips/non_target_videos')
+    for file in non_target_path:
+        full_path = os.path.join('../dataset_stips/non_target_videos', file)
+        if os.path.isfile(full_path):
+            os.remove(full_path)
+
+    for video_dir in os.listdir(path):
+        if video_dir in target_videos:
+            video_path = f'{path}/{video_dir}'
+            destination_dir = '../dataset_stips/target_videos'
+            for all_videos in os.listdir(video_path):
+                file_name = os.path.join(video_path, all_videos)
+                shutil.copy(file_name, destination_dir)
+        elif video_dir == '.DS_Store':
+            continue
+        else:
+            video_path = f'{path}/{video_dir}'
+            destination_dir = '../dataset_stips/non_target_videos'
+            for all_videos in os.listdir(video_path):
+                file_name = os.path.join(video_path, all_videos)
+                shutil.copy(file_name, destination_dir)
 
 
-move_target_videos()
+move_videos()
+move_stips()
+
+create_video_id()
+video_category_map()
+HoG, HoF = get_cluster_representatives()
+target_videos_features()
+non_target_videos_features()
 
 # commit the changes
 connection.commit()
---
+++ Phase 2/Code/Util/Task_2.py
@@ -0,0 +1,120 @@
+import os
+import json
+
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import euclidean_distances
+
+
+# Dictionaries to map (tau, sigma) values to Cluster Representatives
+HOG_centers = {}
+HOF_centers = {}
+
+stip_column_labels = ["point-type", "x", "y", "t", "sigma2", "tau2", "detector-confidence"]
+for i in range(1, 73):
+    stip_column_labels.append("dscr-hog (" + str(i) + ")")
+for i in range(1, 91):
+    stip_column_labels.append("dscr-hof (" + str(i) + ")")
+
+tau_values = [2, 4]
+sigma_values = [4, 8, 16, 32, 64, 128]
+
+
+def get_cluster_representatives():
+    non_target_path = '../../dataset_stips/non_target_videos'
+
+    # Array to store 400 highest confidence STIPs for each non-target video
+    non_target_data = []
+
+    for non_target_videos in os.listdir(non_target_path):
+        video_path = os.path.join(non_target_path, non_target_videos)
+
+        try:
+            temp_df = pd.read_csv(video_path, sep="\t", comment='#', header=None)
+
+            # select 400 highest samples sorted by column index 6 (detector-confidence)
+            temp_df = temp_df.nlargest(400, 6)
+
+            non_target_data.append(temp_df)
+        
+        except pd.errors.EmptyDataError:
+            print("no data in ", non_target_videos)
+            continue
+    
+    # Convert Array to Pandas DataFrame
+    non_target_df = pd.concat(non_target_data, ignore_index=True)
+
+    # Drops the last column as it is an empty column (due to how STIP data provided is formatted)
+    non_target_df = non_target_df.dropna(axis=1)
+    
+    non_target_df.columns = stip_column_labels
+
+
+    for t in tau_values:
+        for s in sigma_values:
+            # Sample 10000 STIPs for each pair of tau2, sigma2 pairs
+            filtered_df = non_target_df[(non_target_df["tau2"] == t) & (non_target_df["sigma2"] == s)].sample(10000)
+
+            # Take the HoG and HoF features
+            X_HOG = filtered_df.loc[:, "dscr-hog (1)":"dscr-hog (72)"]
+            X_HOF = filtered_df.loc[:, "dscr-hof (1)":"dscr-hof (90)"]
+
+            kmeans = KMeans(n_clusters=40)
+
+            kmeans.fit(X_HOG)
+            HOG_centers[str((t, s))] = kmeans.cluster_centers_.tolist()
+
+            kmeans.fit(X_HOF)
+            HOF_centers[str((t, s))] = kmeans.cluster_centers_.tolist()
+
+    # Export the Cluster Representatives
+    with open("HoG_cluster_representatives.json", "w") as fp:
+        json.dump(HOG_centers, fp)
+    with open("HoF_cluster_representatives.json", "w") as fp:
+        json.dump(HOF_centers, fp)
+    
+    return HOG_centers, HOF_centers
+
+
+def find_closest_clusters(x, y):
+    distances = euclidean_distances(x, y)
+    return np.argmin(distances, axis=1)
+
+
+def get_HoG_HoF_features(target_path):
+    try:
+        df = pd.read_csv('../' + target_path, sep="\t", comment='#', header=None)
+        df = df.nlargest(400, 6)
+        df = df.dropna(axis=1)
+        df.columns = stip_column_labels
+    
+    except pd.errors.EmptyDataError:
+        print("no data in ", target_path)
+        return [], []
+
+    hog_histograms = []
+    hof_histograms = []
+
+    for t in tau_values:
+        for s in sigma_values:
+
+            filtered_df = df[(df["tau2"] == t) & (df["sigma2"] == s)]
+            if filtered_df.shape[0] != 0:
+                hog_cluster_Id = find_closest_clusters(filtered_df.loc[:, "dscr-hog (1)":"dscr-hog (72)"], HOG_centers[str((t, s))])
+                hof_cluster_Id = find_closest_clusters(filtered_df.loc[:, "dscr-hof (1)":"dscr-hof (90)"], HOF_centers[str((t, s))])
+
+                hog_histogram, bin_edges = np.histogram(hog_cluster_Id, bins=np.arange(41))
+                hof_histogram, bin_edges = np.histogram(hof_cluster_Id, bins=np.arange(41))
+
+            elif filtered_df.shape[0] != 0:
+                    hog_histograms.append(numpy.zeros(480))
+                    hof_histograms.append(numpy.zeros(480))
+
+            hog_histograms.append(hog_histogram)
+            hof_histograms.append(hof_histogram)
+    
+    bof_HOG_descriptor = np.concatenate(hog_histograms)
+    bof_HOF_descriptor = np.concatenate(hof_histograms)
+
+    return bof_HOG_descriptor, bof_HOF_descriptor
