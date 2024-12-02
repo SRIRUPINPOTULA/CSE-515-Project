3a52f048b7682cb2d03a6bc41744c54a085cfff3
Vibhu Dixit <vibhu.dixit02@gmail.com>
2024-12-01 23:09:02 -0700

    Comments added

--- Phase_3/Code/Task_3_preprocess.py
+++ Phase_3/Code/Task_3_preprocess.py
@@ -2,6 +2,7 @@ import os
 import json
 import cv2
 
+# Function to extract and save the first frame of each video as a thumbnail
 def extract_and_save_thumbnails(json_path, video_directory, output_folder):
     with open(json_path, 'r') as file:
         video_map = json.load(file)
@@ -10,24 +11,25 @@ def extract_and_save_thumbnails(json_path, video_directory, output_folder):
     os.makedirs(output_folder, exist_ok=True)
 
     for action_dir in os.listdir(video_directory):
-        if action_dir == '.gitkeep':
+        if action_dir == '.gitkeep': #for skipping any irrelevant files in the directory
             continue
         
-        action_path = f'{video_directory}/{action_dir}'
+        action_path = f'{video_directory}/{action_dir}' # Construct the path for the current action directory
         for video in os.listdir(action_path):
             video_path = os.path.join(action_path, video)
 
             if video in video_map:
-                video_id = video_map[video]
-                thumbnail_path = os.path.join(output_folder, f"{video_id}.jpg")
+                video_id = video_map[video] # Get the corresponding video ID
+                thumbnail_path = os.path.join(output_folder, f"{video_id}.jpg") # Path for the thumbnail
                 
                 if not os.path.exists(thumbnail_path):
                     cap = cv2.VideoCapture(video_path)
                     success, frame = cap.read()
                     if success:
-                        cv2.imwrite(thumbnail_path, frame)
+                        cv2.imwrite(thumbnail_path, frame)# If the frame is successfully read, save it as a thumbnail
+                    # Release the video capture object
                     cap.release()
-    
+    # Completion message
     print("Thumbnails saved successfully.")
 
 json_path = "../Database/videoID.json"
